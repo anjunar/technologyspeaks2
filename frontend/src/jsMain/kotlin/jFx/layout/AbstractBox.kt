498dfef5fb94af43d6b07199ac694c394e499abc
Patrick Bittner <anjunar@gmx.de>
2026-01-09 08:36:41 +0100

    snapshot

---
+++ frontend/src/jsMain/kotlin/jFx/layout/AbstractBox.kt
@@ -0,0 +1,67 @@
+package jFx.layout
+
+import jFx.core.DSL.ChildNodeBuilder
+import jFx.core.DSL.ElementBuilder
+import jFx.core.DSL.LifeCycle
+import jFx.state.ListProperty
+import kotlinx.browser.document
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
+
+abstract class AbstractBox(val className : String) : ChildNodeBuilder<HTMLDivElement, HTMLElement> {
+
+    private val node by lazy {
+        val divElement = document.createElement("div") as HTMLDivElement
+        if (className.isNotEmpty()) divElement.classList.add()
+        divElement
+    }
+
+    override val children: ListProperty<ElementBuilder<*>> = ListProperty(emptyList())
+    override val applyValues: MutableList<() -> Unit> = mutableListOf()
+
+    override var lifeCycle: LifeCycle = LifeCycle.Build
+
+    private val nodeCache = mutableMapOf<ElementBuilder<*>, HTMLElement>()
+
+    init {
+        children.observe { items ->
+            syncChildren(items)
+        }
+    }
+
+    override fun build(): HTMLDivElement {
+        syncChildren(children.get())
+        return node
+    }
+
+    override fun add(child: ElementBuilder<*>) {
+        children.set(children.get() + child)
+    }
+
+    protected fun syncChildren(newItems: List<ElementBuilder<*>>) {
+        val currentDomNodes = node.childNodes
+
+        val targetNodes = newItems.map { builder ->
+            nodeCache.getOrPut(builder) { builder.build() as HTMLElement }
+        }
+
+        targetNodes.forEachIndexed { index, targetNode ->
+            val currentNodeAtPosition = currentDomNodes.item(index)
+
+            if (currentNodeAtPosition == null) {
+                node.appendChild(targetNode)
+            } else if (currentNodeAtPosition != targetNode) {
+                node.insertBefore(targetNode, currentNodeAtPosition)
+            }
+        }
+
+        while (node.childNodes.length > targetNodes.size) {
+            node.lastChild?.let { node.removeChild(it) }
+        }
+
+        val itemSet = newItems.toSet()
+        nodeCache.keys.retainAll(itemSet)
+    }
+
+}
+
--- frontend/src/jsMain/kotlin/jFx/layout/Div.kt
+++ frontend/src/jsMain/kotlin/jFx/layout/Div.kt
@@ -1,31 +1,8 @@
 package jFx.layout
 
-import jFx.core.DSL.ChildNodeBuilder
-import jFx.core.DSL.ElementBuilder
-import jFx.core.DSL.LifeCycle
 import jFx.core.DSL.ParentScope
-import jFx.state.ListProperty
-import kotlinx.browser.document
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.Node
 
-class Div : ChildNodeBuilder<HTMLDivElement, Node> {
-    val node by lazy { document.createElement("div") as HTMLDivElement }
-
-    override val children = ListProperty<ElementBuilder<*>>()
-
-    override fun build(): HTMLDivElement = node
-
-    override fun add(child: ElementBuilder<*>) {
-        val built = child.build()
-        if (built is Node) {
-            node.appendChild(built)
-        }
-    }
-
-    override val applyValues = mutableListOf<() -> Unit>()
-    override var lifeCycle = LifeCycle.Build
-
+class Div : AbstractBox("") {
     companion object {
         fun ParentScope.div(body: Div.() -> Unit): Div {
             val builder = Div()
