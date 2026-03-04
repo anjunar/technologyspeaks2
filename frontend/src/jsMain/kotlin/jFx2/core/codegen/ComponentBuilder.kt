4c4a9ccbf10b4502b0f26851ee0345da23aaf111
Patrick Bittner <anjunar@gmx.de>
2026-03-04 06:14:33 +0100

    removed : comments

--- frontend/src/jsMain/kotlin/jFx2/core/codegen/ComponentBuilder.kt
+++ frontend/src/jsMain/kotlin/jFx2/core/codegen/ComponentBuilder.kt
@@ -3,23 +3,13 @@ package jFx2.core.codegen
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class JfxComponentBuilder(
-    /**
-     * Optional explicit builder function name.
-     * Defaults to the class name with a lower-cased first character (e.g. `LoggedInUser` -> `loggedInUser`).
-     */
+
     val name: String = "",
-    /**
-     * HTML tag name used for `scope.create(...)`.
-     * If empty, the processor will try to derive it from the `node` constructor parameter type.
-     */
+
     val tag: String = "",
-    /**
-     * CSS classes to add to the created element.
-     */
+
     val classes: Array<String> = [],
-    /**
-     * How to invoke `afterBuild()` (if present on the component).
-     */
+
     val afterBuild: AfterBuildMode = AfterBuildMode.SCHEDULED
 )
 
